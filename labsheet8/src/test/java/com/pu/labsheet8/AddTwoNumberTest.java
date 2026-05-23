98b98d7c097ccd7d986f445cb54c6a93cf829692
MShiri9392 <shiri957313@gmail.com>
2026-05-23 11:57:13 +0530

    Refactor AddTwoNumberTest and add multiplication/division tests

--- labsheet8/src/test/java/com/pu/labsheet8/AddTwoNumberTest.java
+++ labsheet8/src/test/java/com/pu/labsheet8/AddTwoNumberTest.java
@@ -2,33 +2,32 @@ package com.pu.labsheet8;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 public class AddTwoNumberTest {
-	@Test
-
-	void testAdd()
-
-	{
-
-	AddTwoNumber calc = new AddTwoNumber();
-
-	int result = calc.add(2, 3);
-
-	assertEquals(5, result, "Addition should return 5");
-
-	}
-
-	@Test
-
-	void testSubtract()
-
-	{
-
-	AddTwoNumber calc = new AddTwoNumber();
-
-	int result = calc.subtract(2, 3);
-
-	assertEquals(-1, result, "Subtraction should return -1");
-
-	}
-
-
+    @Test
+    void testAdd()
+ {
+        AddTwoNumber calc = new AddTwoNumber();
+        int result = calc.add(2, 3);
+        assertEquals(5, result, "Addition should return 5");
+    }
+    @Test
+    void testSubtract() 
+{
+    	AddTwoNumber calc = new AddTwoNumber();
+        int result = calc.subtract(2, 3);
+        assertEquals(-1, result, "Subtraction should return -1");
+    }
+    @Test
+    void testMultiplication() 
+{
+    	AddTwoNumber calc = new AddTwoNumber();
+        int result = calc.Multiplication(2, 3);
+        assertEquals(6, result, "Multiplication should return 6");
+}
+    @Test
+    void testDiv() 
+{
+    	AddTwoNumber calc = new AddTwoNumber();
+        int result = calc.div(6, 3);
+        assertEquals(2, result, "Division should return 2");
+}       
 }
